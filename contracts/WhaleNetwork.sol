da4a5172e35aef8f24f6cfa83876e405896f0e0b
Parth Oberoi <htrapdev@gmail.com>
2017-11-17 21:37:14 +0530

    wip whalecontracts v3

--- contracts/WhaleNetwork.sol
+++ contracts/WhaleNetwork.sol
@@ -2,52 +2,28 @@ pragma solidity ^0.4.11;
 
 
 contract WhaleNetwork {
-
-  event Posted(
-      address author,
-      string title,
-      uint id
-  );
+//compiling now
 
   struct Whale {
     uint id;
-    uint[] whaleBlocks;
-    uint[] normalBlocks;
-    uint shares;
+    uint shares; // keeps track of the whale shares
     bool isWhale;
-    bool validated;
-    uint[] postList;
+    uint lastBlockShared;
   }
 
-  struct Validator {
-    uint timestamp;
-    bool isValidator;
-  }
-
-  struct Post {
-    uint timestamp;
-    uint id;
-    address[] followers;
-    address whale;
-    string title;
-  }
-
-  struct Vars {
-    uint i;
-  }
-
-  uint public numPosts;
-  mapping (uint => Post) public posts;
   address owner;
   mapping (address => Whale) public whales;
   uint public numWhales;
   uint whaleRequirement;
-  address[] public whaleList;
-  mapping (address => Validator) validators;
-  mapping (address => address) public whaleMod;
+  address[] public whaleList; //we would need this to show a list of whales
+
+  // keep track of the networkShares
   uint public networkShares;
+  // keeps track of the last block that the network share was recorded
   uint lastBlockShareRecorded;
-  mapping (address => uint) lastWhaleShareRecorded;
+  // keeps track of the last block that the whale shere was recorded
+  uint id;
+
   //modifiers
 
   modifier isOwner() {
@@ -56,127 +32,68 @@ contract WhaleNetwork {
   }
 
   modifier onlyWhale() {
-      require(whales[msg.sender].isWhale);
-      _;
-  }
-
-  modifier onlyValidators() {
-      require(validators[msg.sender].isValidator);
-      _;
+    require(whales[msg.sender].isWhale);
+    _;
   }
 
   function WhaleNetwork(address _owner) {
     owner = _owner;
     numWhales = 0;
     whaleRequirement = 100;
-    numPosts = 0;
   }
 
-
   //setter functions
-
   function becomeWhale() payable {
     require(msg.value == whaleRequirement);
     require(!whales[msg.sender].isWhale);
-    whales[msg.sender].whaleBlocks.push(block.number);
     whales[msg.sender].isWhale = true;
+    whales[msg.sender].id = id; // now assigned here
+    whales[msg.sender].lastBlockShared = block.number;
     whaleList.push(msg.sender);
     numWhales++;
+    id++;
   }
 
   function becomeNormal() {
     require(whales[msg.sender].isWhale);
-    whales[msg.sender].normalBlocks.push(block.number);
-    whales[msg.sender].shares += (block.number - lastWhaleShareRecorded[msg.sender]);
-    lastWhaleShareRecorded[msg.sender] =  block.number;
+    whales[msg.sender].shares += (block.number - whales[msg.sender].lastBlockShared);
+    whales[msg.sender].lastBlockShared = block.number;
     whales[msg.sender].isWhale = false;
-    whaleList[whales[msg.sender].id] = whaleList[whaleList.length-1];
-    delete whaleList[whaleList.length-1];
     numWhales--;
     networkShares += numWhales * (block.number - lastBlockShareRecorded);
     lastBlockShareRecorded = block.number;
     msg.sender.transfer(whaleRequirement);
   }
 
-  function designateModerator(address mod) onlyWhale{
-      whaleMod[msg.sender] = mod;
-  }
-
-  function validate(address whale) onlyValidators {
-      require(whales[whale].isWhale); //check if whale
-      whales[whale].validated = true;
-  }
-
-  function addValidator(address addr) isOwner{
-      require(!validators[addr].isValidator);
-      validators[addr].timestamp = now;
-      validators[addr].isValidator = true;
-  }
-
-  function post(string postTitle, address whale)  {
-    require(whales[whale].isWhale);
-    require(whaleMod[whale] == msg.sender);
-    require(bytes(postTitle).length <= 160);
-    posts[numPosts].id = numPosts;
-    posts[numPosts].timestamp = now;
-    posts[numPosts].whale = whale;
-    posts[numPosts].title = postTitle;
-    whales[msg.sender].postList.push(numPosts);
-    Posted(msg.sender, postTitle, numPosts);
-    numPosts++;
-  }
-
-  function addFollowers(uint postid, address[] followers) {
-    Vars memory vars;
-    require(whaleMod[posts[postid].whale] == msg.sender);
-    for (vars.i=0; vars.i<followers.length; vars.i++) {
-      posts[postid].followers.push(followers[vars.i]);
-  }
-  }
-
-
-  //getter functions
-  function getWhaleShare(address _address) returns (uint shares) {
+  function updateWhaleShare(address _address) {
     if (whales[_address].isWhale) {
-      whales[_address].shares += (block.number - lastWhaleShareRecorded[_address]);
-      lastWhaleShareRecorded[_address] = block.number;
+      whales[_address].shares += (block.number - whales[_address].lastBlockShared);
+      whales[_address].lastBlockShared = block.number;
     }
-    return whales[_address].shares;
   }
 
-  function getNetworkShare() returns (uint) {
+  function updateNetworkShare() {
     networkShares += (block.number - lastBlockShareRecorded) * numWhales;
     lastBlockShareRecorded = block.number;
-    return networkShares;
   }
 
   function isWhale(address _address) constant returns (bool _status) {
-    bool status = whales[_address].isWhale;
-    return status;
+    return whales[_address].isWhale;
   }
 
   function getLockedBalance() constant returns (uint balance) {
     balance = this.balance;
   }
 
-  function getWhale(address _addr) public constant returns (uint num, address moderator, uint shares) {
-      return (whales[_addr].postList.length, whaleMod[_addr], whales[_addr].shares);
+  function getWhaleShares(address _addr) public constant returns (uint shares) {
+    return whales[_addr].shares;
   }
 
-  function getPost(uint postId) constant returns (uint id, uint timestamp, address whale, uint numFollowers) {
-    require(postId < numPosts); //We check that the post exists
-    id = posts[postId].id;
-    timestamp = posts[postId].timestamp;
-    whale = posts[postId].whale;
-    numFollowers = posts[postId].followers.length;
+  function getWhaleLastBlockShared(address _addr) public constant returns (uint blocks) {
+    return whales[_addr].lastBlockShared;
   }
 
-  function getFollower(uint pid, uint fid) constant returns (address) {
-    return posts[pid].followers[fid];
+  function getWhaleNextBlockShared(address _addr) public constant returns (uint blocks) {
+    return whales[_addr].lastBlockShared + 1000;
   }
-
-  function getCurrentWhaleCount() public constant returns(uint count) {
-    return whaleList.length;
-}
-
 }
--- contracts/WhaleRewards.sol
+++ contracts/WhaleRewards.sol
@@ -13,53 +13,53 @@ contract WhaleRewards{
     uint whaleShare;
     uint unclaimedShare;
     uint reward;
-
   }
 
   event Claimed(
     uint reward,
-    address follower
+    address whale
     );
 
   WhaleNetwork whaleNetwork;
   /*uint distEpoch;
   uint distBloc;*/
   function WhaleRewards() {
-      owner = msg.sender;
-      whaleNetwork = new WhaleNetwork(owner);
-      networkAddress = address(whaleNetwork);
-    }
+    owner = msg.sender;
+    whaleNetwork = new WhaleNetwork(owner);
+    networkAddress = address(whaleNetwork);
+  }
 
   modifier isOwner() {
-      require(owner==msg.sender);
-      _;
-    }
+    require(owner==msg.sender);
+    _;
+  }
 
-    //Reward functions
-    function () payable {
+  //Reward functions
+  function () payable {
+  }
 
-    }
+  function claimReward(address addr) {
+    require(block.number - whaleNetwork.getWhaleLastBlockShared(addr) >= 1000);
+    Vars memory vars;
+    // these functions update the networkshare and the whale share and change the state
+    // this is the reason the blocks mined increase on claiming reward
+    whaleNetwork.updateNetworkShare();
+    whaleNetwork.updateWhaleShare(addr);
+    // These are public getters that get the state variables // like here
+    vars.netShare = whaleNetwork.networkShares();
+    vars.whaleShare = whaleNetwork.getWhaleShares(addr);
+    // whaleshare is the total share whale has accumulated on the platform
+    // claimedShare is the state variable that stores the claimed whale shares
+    vars.unclaimedShare = vars.whaleShare - claimedShare[addr];
+    vars.reward = (vars.unclaimedShare * this.balance) / vars.netShare;
+    claimedShare[addr] += vars.unclaimedShare;
+    addr.transfer(vars.reward);
+    // event that is triggered which I wait for and show it as an alert in the ui
+    Claimed(vars.reward, addr);
+  }
 
-
-
-    function claimReward(address addr) {
-      Vars memory vars;
-      whaleNetwork.getNetworkShare();
-      whaleNetwork.getWhaleShare(addr);
-      vars.netShare = whaleNetwork.networkShares();
-      (,,vars.whaleShare) = whaleNetwork.getWhale(addr);
-      vars.unclaimedShare = vars.whaleShare - claimedShare[addr];
-      vars.reward = (vars.unclaimedShare * this.balance) / vars.netShare;
-      claimedShare[addr] += vars.unclaimedShare;
-      addr.transfer(vars.reward);
-      Claimed(vars.reward, addr);
-
-    }
-
-
-    function getNetworkAddress() constant returns (address addr){
-      return networkAddress;
-    }
-
+  function getNetworkAddress() constant returns (address addr){
+    return networkAddress;
+  }
 
 }
