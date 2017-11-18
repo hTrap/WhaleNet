d478dff6185cfaf87209909c0203cb86834f17fa
Parth Oberoi <htrapdev@gmail.com>
2017-11-19 01:02:35 +0530

    fix rewards claim for old contract in v2 harcoded 240 whale addresses

--- src/components/forms/rewardClaim.js
+++ src/components/forms/rewardClaim.js
@@ -96,7 +96,7 @@ class RewardClaim extends Component {
           from: this.state.address
         }
 
-        var rawTx = txutils.functionTx(whaleClaimV2Instance.abi, 'claimReward',[this.state.follower], txOptions);
+        var rawTx = txutils.functionTx(whaleClaimV2Instance.abi, 'claimReward',[this.state.follower.toLowerCase()], txOptions);
         console.log(1)
         var privateKey = new Buffer(this.state.privateKey, 'hex');
         var transaction = new tx(rawTx);
