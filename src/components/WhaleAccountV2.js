1a55be8cc7ad7de4dfeebde0253bb272ae1fa932
Parth Oberoi <htrapdev@gmail.com>
2017-11-18 22:10:54 +0530

    Adding check whale to version 3

--- src/components/WhaleAccountV2.js
+++ src/components/WhaleAccountV2.js
@@ -9,9 +9,10 @@ import getWeb3 from '../utils/getWeb3'
 import Button from 'material-ui/Button';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import ReactDOM from 'react-dom'
-import BecomeWhaleFormV2 from '../components/forms/BecomeWhaleFormV2.js'
-import BecomeNormalFormV2 from '../components/forms/BecomeNormalFormV2.js'
-import Header from '../components/header.js'
+import BecomeWhaleFormV2 from './forms/BecomeWhaleFormV2.js'
+import BecomeNormalFormV2 from './forms/BecomeNormalFormV2.js'
+import WhaleCheckFormV2 from './forms/WhaleCheckFormV2.js'
+import Header from './header.js'
 import TextField from 'material-ui/TextField';
 
 
@@ -59,7 +60,12 @@ class WhaleAccountV2 extends Component {
           <div className={this.props.classes.root}>
 
           <Grid container spacing={24}>
+          <Grid item xs={12}>
+            <Paper className={this.props.classes.paper}>
 
+            {<WhaleCheckFormV2/>}
+            </Paper>
+          </Grid>
             <Grid item xs={12} sm={6}>
               <Paper className={this.props.classes.paper}>
 
